7cc9107e872098c43d1ce547718704df79ad30c0
Joanne Ng <joanne.ngzy.is@gmail.com>
2020-08-16 00:39:19 +1000

    moving platforms

--- simple_platformer.py
+++ simple_platformer.py
@@ -99,9 +99,15 @@ class platform(pygame.sprite.Sprite):
                                                  random.randint(0, HEIGHT-30)))
         self.moving = True
         self.point = True 
+        self.speed = random.randint(-1, 1)
  
     def move(self):
-        pass
+        if self.moving == True:  
+            self.rect.move_ip(self.speed, 0)
+            if self.speed > 0 and self.rect.left > WIDTH:
+                self.rect.right = 0
+            if self.speed < 0 and self.rect.right < 0:
+                self.rect.left = WIDTH
  
  
 def check(platform, groupies):
